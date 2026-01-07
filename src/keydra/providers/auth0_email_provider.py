386560f61f5d8b854b07d2eb8de057165699f9a6
Mitchell Murphy <mitchell.murphy@athena.com.au>
2026-01-07 22:04:57 +1100

    Remove debug print statements from email credentials distribution method

--- src/keydra/providers/auth0_email_provider.py
+++ src/keydra/providers/auth0_email_provider.py
@@ -20,17 +20,11 @@ class Client(BaseProvider):
         print(self.auth0_client._token)
         
     def _distribute_email_credentials(self, secret, destination):
-        print("destination")
-        print(destination)
-        print("destination")
-        
         post_data = dict()
         
         # Add the mapped values from the secret
         for mapdest, mapsrc in destination['source'].items():
             post_data[mapdest] = secret[mapsrc]
-            
-        print("post_data", post_data)
         
         self.auth0_client.update_email_provider(post_data)
         return destination
